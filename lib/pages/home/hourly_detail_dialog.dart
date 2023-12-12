d94d092d1d5b2573481871bb8615b2b1b75bf5bb
wangkz <wangkz@infore.com>
2023-12-12 11:43:04 +0800

    feat(逐小时预报详情弹窗): 屏幕适配

--- lib/pages/home/hourly_detail_dialog.dart
+++ lib/pages/home/hourly_detail_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_good_weather/bean/hourly_weather_bean.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../constant/constant.dart';
@@ -23,13 +24,12 @@ class HourlyDetailDialog extends StatelessWidget {
             // 阴影颜色
             shadowColor: Colors.deepOrange,
             // 阴影高度
-            elevation: 8,
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8))),
+            elevation: 8.h,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.w))),
             // 是否在 child 前绘制 border，默认为 true
             borderOnForeground: true,
             // 外边距
-            margin: const EdgeInsets.all(50),
+            margin: EdgeInsets.all(50.w),
             child: Column(
               children: [
                 Stack(
@@ -38,23 +38,23 @@ class HourlyDetailDialog extends StatelessWidget {
                     Container(
                       width: double.infinity,
                       alignment: Alignment.topCenter,
-                      margin: const EdgeInsets.only(top: 20),
-                      child: const Text(
+                      margin: EdgeInsets.only(top: 20.h),
+                      child: Text(
                         "逐小时预报详情",
                         style: TextStyle(
                           color: Colors.black,
-                          fontSize: 16,
+                          fontSize: 20.sp,
                         ),
                       ),
                     ),
                     Positioned(
-                      top: 20,
-                      right: 20,
+                      top: 20.h,
+                      right: 20.w,
                       child: GestureDetector(
                         child: SvgPicture.asset(
                           "${Constant.assetsSvg}ic_close.svg",
-                          width: 24,
-                          height: 24,
+                          width: 24.w,
+                          height: 24.w,
                           fit: BoxFit.cover,
                         ),
                         onTap: () {
@@ -67,211 +67,67 @@ class HourlyDetailDialog extends StatelessWidget {
                 Container(
                   width: double.infinity,
                   alignment: Alignment.topCenter,
-                  margin: const EdgeInsets.only(top: 8),
+                  margin: EdgeInsets.only(top: 8.h),
                   child: Text(
                     divideTime(updateTime(hourly?.fxTime)),
-                    style: const TextStyle(
+                    style: TextStyle(
                       color: Colors.black,
-                      fontSize: 18,
+                      fontSize: 20.sp,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Container(
                   width: double.infinity,
-                  height: 200,
-                  margin: const EdgeInsets.only(left: 60, top: 8, right: 60),
+                  height: 280.h,
+                  margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 8.h),
                   child: GridView(
                     padding: EdgeInsets.zero,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       // 横轴2个子widget
                       crossAxisCount: 2,
                       // 宽高比为6
                       childAspectRatio: 6,
                     ),
                     children: [
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "温度",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.temp}℃"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "天气状况",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.text}"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "风向360角度",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.wind360}°"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "风向",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.windDir}"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "风力",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.windScale}级"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "风速",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.windSpeed}公里/小时"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "云量",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.cloud}%"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "相对湿度",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.humidity}%"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "大气压强",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.pressure}hPa"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "降水概率",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.pop}%"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        margin: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "露点温度",
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 14),
-                            ),
-                            Text("${hourly?.dew}℃"),
-                          ],
-                        ),
-                      ),
+                      buildHourlyItem("温度", "${hourly?.temp}℃"),
+                      buildHourlyItem("天气状况", "${hourly?.text}"),
+                      buildHourlyItem("风向360角度", "${hourly?.wind360}°"),
+                      buildHourlyItem("风向", "${hourly?.windDir}"),
+                      buildHourlyItem("风力等级", "${hourly?.windScale}级"),
+                      buildHourlyItem("风速", "${hourly?.windSpeed}公里/小时"),
+                      buildHourlyItem("相对湿度", "${hourly?.humidity}%"),
+                      buildHourlyItem("当前小时累计降水量", "${hourly?.precip}mm"),
+                      buildHourlyItem("降水概率", "${hourly?.pop}%"),
+                      buildHourlyItem("大气压强", "${hourly?.pressure}hPa"),
+                      buildHourlyItem("云量", "${hourly?.cloud}%"),
+                      buildHourlyItem("露点温度", "${hourly?.dew}℃"),
                     ],
                   ),
                 )
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildHourlyItem(String name, String value) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            name,
+            style: TextStyle(color: Colors.black, fontSize: 14.sp),
+          ),
+          Text(
+            value,
+            style: TextStyle(color: Colors.black, fontSize: 30.sp),
           ),
         ],
       ),
