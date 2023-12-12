613cf4cb521c449c1b1f9e2de719c537fb0981ed
wangkz <wangkz@infore.com>
2023-12-12 11:56:43 +0800

    feat(管理城市): 屏幕适配

--- lib/pages/city/manage_city_page.dart
+++ lib/pages/city/manage_city_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_good_weather/bean/my_city_bean.dart';
 import 'package:flutter_good_weather/db/my_city_dao.dart';
 import 'package:flutter_good_weather/widget/title_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../constant/constant.dart';
@@ -33,12 +34,12 @@ class _ManageCityPageState extends State<ManageCityPage> {
         },
       ),
       body: Container(
-        color: Colors.grey.withAlpha(88),
+        color: const Color(0xfff5f5f5),
         child: Column(
           children: [
             Expanded(
               child: Container(
-                margin: const EdgeInsets.only(top: 16),
+                margin: EdgeInsets.only(top: 16.h),
                 child: ListView.separated(
                   itemBuilder: (context, index) {
                     return Wrap(
@@ -49,33 +50,28 @@ class _ManageCityPageState extends State<ManageCityPage> {
                           // 阴影颜色
                           shadowColor: Colors.grey,
                           // 阴影高度
-                          elevation: 4,
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8))),
+                          elevation: 4.h,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.w))),
                           // 是否在 child 前绘制 border，默认为 true
                           borderOnForeground: true,
                           // 外边距
-                          margin: const EdgeInsets.only(
-                              left: 16, right: 16, bottom: 8),
+                          margin: EdgeInsets.only(left: 16.w, right: 16.w, bottom: 8.h),
                           child: Material(
                             color: Colors.transparent,
                             child: InkWell(
                               child: Row(
                                 children: [
                                   Container(
-                                    margin: const EdgeInsets.all(16),
+                                    margin: EdgeInsets.all(16.w),
                                     child: Text(
                                       myCityList?[index].cityName ?? "",
-                                      style: const TextStyle(
-                                          color: Colors.black, fontSize: 16),
+                                      style: TextStyle(color: Colors.black, fontSize: 16.sp),
                                     ),
                                   ),
                                 ],
                               ),
                               onTap: () {
-                                Navigator.pop(
-                                    context, myCityList?[index].cityName);
+                                Navigator.pop(context, myCityList?[index].cityName);
                               },
                             ),
                           ),
@@ -84,10 +80,7 @@ class _ManageCityPageState extends State<ManageCityPage> {
                     );
                   },
                   separatorBuilder: (context, index) {
-                    return const Divider(
-                      height: 0,
-                      color: Colors.transparent,
-                    );
+                    return const Divider(height: 0, color: Colors.transparent);
                   },
                   itemCount: myCityList?.length ?? 0,
                 ),
@@ -98,32 +91,28 @@ class _ManageCityPageState extends State<ManageCityPage> {
               child: InkWell(
                 child: SizedBox(
                   width: double.infinity,
-                  height: 60,
+                  height: 60.h,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       SvgPicture.asset(
                         "${Constant.assetsSvg}ic_add.svg",
-                        width: 24,
-                        height: 24,
+                        width: 24.w,
+                        height: 24.w,
                         color: Colors.black,
                       ),
-                      const Text(
+                      Text(
                         "添加城市",
-                        style: TextStyle(color: Colors.black, fontSize: 12),
+                        style: TextStyle(color: Colors.black, fontSize: 12.sp),
                       )
                     ],
                   ),
                 ),
                 onTap: () {
-                  CityPickers.showCitiesSelector(context: context)
-                      .then((result) {
-                    if (result?.cityName != null &&
-                        result?.cityName?.isNotEmpty == true) {
-                      MyCityDao.getInstance().insert([
-                        MyCityBean(cityName: result?.cityName)
-                      ]).then((value) {
+                  CityPickers.showCitiesSelector(context: context).then((result) {
+                    if (result?.cityName != null && result?.cityName?.isNotEmpty == true) {
+                      MyCityDao.getInstance().insert([MyCityBean(cityName: result?.cityName)]).then((value) {
                         getAllCityData();
                       });
                     }
