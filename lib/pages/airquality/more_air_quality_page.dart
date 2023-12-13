91699091bd413637f26f9211681b8f2074e937a4
wangkz <wangkz@infore.com>
2023-12-13 13:51:32 +0800

    feat(更多空气质量): 屏幕适配

--- lib/pages/airquality/more_air_quality_page.dart
+++ lib/pages/airquality/more_air_quality_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_good_weather/bean/air_quality_forecast_bean.dart';
 import 'package:flutter_good_weather/http/api/api.dart';
 import 'package:flutter_good_weather/util/screen_util.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 import '../../bean/air_quality_bean.dart';
@@ -65,16 +66,16 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
             ),
           ),
           Positioned.fill(
-              top: navigationBarHeight + 12,
-              bottom: 20,
+              top: navigationBarHeight + 12.h,
+              bottom: 20.h,
               child: CustomScrollView(
                 slivers: [
                   // 实时空气质量
                   buildAirQualityNow(),
-                  const SliverPadding(padding: EdgeInsets.only(top: 12)),
+                  SliverPadding(padding: EdgeInsets.only(top: 12.h)),
                   // 监测站空气质量
                   buildAirQualityStation(),
-                  const SliverPadding(padding: EdgeInsets.only(top: 12)),
+                  SliverPadding(padding: EdgeInsets.only(top: 12.h)),
                   // 未来5天空气质量预报
                   buildAirQualityForecast(),
                 ],
@@ -90,25 +91,25 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 12),
+          Padding(
+            padding: EdgeInsets.only(left: 12.w),
             child: Text(
               "未来5天空气质量预报",
-              style: TextStyle(color: Colors.white, fontSize: 16),
+              style: TextStyle(color: Colors.white, fontSize: 16.sp),
             ),
           ),
-          const SizedBox(height: 8),
+          SizedBox(height: 8.h),
           Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12),
+            margin: EdgeInsets.symmetric(horizontal: 12.w),
             width: double.infinity,
-            height: 138,
+            height: 138.h,
             child: ListView.separated(
               scrollDirection: Axis.horizontal,
               itemBuilder: (BuildContext context, int index) {
                 return buildAirQualityForecastItem(airQualityForecastBean?.daily?[index]);
               },
               separatorBuilder: (context, index) {
-                return const VerticalDivider(width: 12, color: Colors.transparent);
+                return VerticalDivider(width: 12.w, color: Colors.transparent);
               },
               itemCount: airQualityForecastBean?.daily?.length ?? 0,
             ),
@@ -122,59 +123,57 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
   Container buildAirQualityForecastItem(Daily? daily) {
     var fxDate = daily?.fxDate;
     return Container(
-      width: 180,
+      width: 180.w,
       height: double.infinity,
       decoration: BoxDecoration(
         color: const Color(0x66000000),
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.w),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(12),
+        padding: EdgeInsets.all(12.w),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(fxDate != null ? friendlyWeekDay(fxDate) : "",
-                style: const TextStyle(color: Colors.white, fontSize: 18)),
+            Text(fxDate != null ? friendlyWeekDay(fxDate) : "", style: TextStyle(color: Colors.white, fontSize: 18.sp)),
             Text(fxDate != null ? formatDate(DateTime.parse(fxDate), formats: ["mm", "/", "dd"]) : "",
-                style: const TextStyle(color: Colors.white, fontSize: 14)),
+                style: TextStyle(color: Colors.white, fontSize: 14.sp)),
             Row(
               children: [
-                const Expanded(
+                Expanded(
                   flex: 1,
-                  child: Center(child: Text("AQI指数", style: TextStyle(color: Colors.white, fontSize: 14))),
+                  child: Center(child: Text("AQI指数", style: TextStyle(color: Colors.white, fontSize: 14.sp))),
                 ),
                 Expanded(
                   flex: 1,
-                  child:
-                      Center(child: Text(daily?.aqi ?? "", style: const TextStyle(color: Colors.white, fontSize: 14))),
+                  child: Center(child: Text(daily?.aqi ?? "", style: TextStyle(color: Colors.white, fontSize: 14.sp))),
                 ),
               ],
             ),
             Row(
               children: [
-                const Expanded(
+                Expanded(
                   flex: 1,
-                  child: Center(child: Text("空气质量", style: TextStyle(color: Colors.white, fontSize: 14))),
+                  child: Center(child: Text("空气质量", style: TextStyle(color: Colors.white, fontSize: 14.sp))),
                 ),
                 Expanded(
                   flex: 1,
                   child: Center(
-                      child: Text(daily?.category ?? "", style: const TextStyle(color: Colors.white, fontSize: 14))),
+                      child: Text(daily?.category ?? "", style: TextStyle(color: Colors.white, fontSize: 14.sp))),
                 ),
               ],
             ),
             Row(
               children: [
-                const Expanded(
+                Expanded(
                   flex: 1,
-                  child: Center(child: Text("污染物", style: TextStyle(color: Colors.white, fontSize: 14))),
+                  child: Center(child: Text("污染物", style: TextStyle(color: Colors.white, fontSize: 14.sp))),
                 ),
                 Expanded(
                   flex: 1,
                   child: Center(
                     child: Text(
                       "NA" == daily?.primary ? "无污染" : daily?.primary ?? "",
-                      style: const TextStyle(color: Colors.white, fontSize: 14),
+                      style: TextStyle(color: Colors.white, fontSize: 14.sp),
                     ),
                   ),
                 ),
@@ -192,26 +191,24 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 12),
+          Padding(
+            padding: EdgeInsets.only(left: 12.w),
             child: Text(
               "监测站空气质量",
-              style: TextStyle(color: Colors.white, fontSize: 16),
+              style: TextStyle(color: Colors.white, fontSize: 16.sp),
             ),
           ),
-          const SizedBox(height: 8),
+          SizedBox(height: 8.h),
           // 监测站点
           Padding(
-            padding: const EdgeInsets.only(left: 12),
+            padding: EdgeInsets.only(left: 12.w),
             child: SizedBox(
               width: double.infinity,
-              height: 238,
+              height: 238.h,
               child: PageView.builder(
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.only(
-                      right: 12,
-                    ),
+                    padding: EdgeInsets.only(right: 12.w),
                     child: buildAirQualityStationItem(airQualityBean?.station?[index]),
                   );
                 },
@@ -227,20 +224,20 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
   /// 监测站item
   Container buildAirQualityStationItem(Station? station) {
     return Container(
-      height: 200,
+      height: 200.h,
       decoration: BoxDecoration(
         color: const Color(0x66000000),
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(12.w),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(12),
+        padding: EdgeInsets.all(12.w),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
               station?.name ?? "",
-              style: const TextStyle(color: Colors.white, fontSize: 18),
+              style: TextStyle(color: Colors.white, fontSize: 18.sp),
             ),
             buildAirQualityStationContentItem("空气质量", station?.category ?? ""),
             buildAirQualityStationContentItem("空气质量指数", station?.aqi ?? ""),
@@ -268,7 +265,7 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
             child: Center(
               child: Text(
                 name,
-                style: const TextStyle(color: Color(0xff9FC8E9), fontSize: 12),
+                style: TextStyle(color: Color(0xff9FC8E9), fontSize: 12.sp),
               ),
             ),
           ),
@@ -277,7 +274,7 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
             child: Center(
               child: Text(
                 value,
-                style: const TextStyle(color: Colors.white, fontSize: 12),
+                style: TextStyle(color: Colors.white, fontSize: 12.sp),
               ),
             ),
           ),
@@ -288,30 +285,30 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
   SliverToBoxAdapter buildAirQualityNow() {
     return SliverToBoxAdapter(
       child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 12),
+        margin: EdgeInsets.symmetric(horizontal: 12.w),
         decoration: BoxDecoration(
           color: const Color(0x66000000),
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(12.w),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 12.0.h),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Text(
+                  Text(
                     "实时空气质量",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
+                    style: TextStyle(color: Colors.white, fontSize: 16.sp),
                   ),
                   Text(
                     "上次更新时间：${divideTime(updateTime(airQualityBean?.updateTime))}",
-                    style: const TextStyle(color: Colors.white, fontSize: 12),
+                    style: TextStyle(color: Colors.white, fontSize: 12.sp),
                   ),
                 ],
               ),
-              Container(height: 8),
+              SizedBox(height: 8.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -321,23 +318,17 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text(
+                      Text(
                         "污染指数",
-                        style: TextStyle(color: Colors.white, fontSize: 14),
+                        style: TextStyle(color: Colors.white, fontSize: 14.sp),
                       ),
-                      const SizedBox(
-                        width: 1,
-                        child: Divider(
-                          color: Colors.transparent,
-                          thickness: 8,
-                        ),
-                      ),
+                      SizedBox(height: 8.h),
                       SleekCircularSlider(
                         appearance: CircularSliderAppearance(
                           customWidths: CustomSliderWidths(
-                            trackWidth: 8,
-                            progressBarWidth: 8,
-                            shadowWidth: 20,
+                            trackWidth: 8.w,
+                            progressBarWidth: 8.w,
+                            shadowWidth: 20.w,
                           ),
                           customColors: CustomSliderColors(
                             trackColor: Colors.grey,
@@ -349,20 +340,20 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
                           infoProperties: InfoProperties(
                             // 空气质量描述 取值范围：优，良，轻度污染，中度污染，重度污染，严重污染
                             topLabelText: airQualityBean?.now?.category,
-                            topLabelStyle: const TextStyle(
+                            topLabelStyle: TextStyle(
                               color: Colors.white,
-                              fontSize: 24,
+                              fontSize: 24.sp,
                               fontWeight: FontWeight.w400,
                             ),
                             mainLabelStyle:
-                                const TextStyle(color: Colors.white, fontSize: 28, fontWeight: FontWeight.w400),
+                                TextStyle(color: Colors.white, fontSize: 28.sp, fontWeight: FontWeight.w400),
                             modifier: (value) {
                               return "${value.toInt()}";
                             },
                           ),
                           startAngle: 135,
                           angleRange: 270,
-                          size: 120,
+                          size: 0.25.sw,
                           animationEnabled: true,
                         ),
                         min: 0,
@@ -421,37 +412,37 @@ class _MoreAirQualityPageState extends State<MoreAirQualityPage> {
   /// 实时空气质量指标item
   Widget buildAirQualityNowItem(String airQualityTitle, String? airQualityValue) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
+      padding: EdgeInsets.symmetric(vertical: 4.h),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
-            width: 48,
+            width: 48.w,
             child: Center(
               child: Text(
                 airQualityTitle,
-                style: const TextStyle(color: Color(0xff9FC8E9), fontSize: 12),
+                style: TextStyle(color: const Color(0xff9FC8E9), fontSize: 12.sp),
               ),
             ),
           ),
           SizedBox(
-            width: 120,
-            height: 8,
+            width: 120.w,
+            height: 8.h,
             child: LinearProgressIndicator(
               backgroundColor: const Color(0xffC6D7F4),
               color: Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              borderRadius: BorderRadius.all(Radius.circular(5.w)),
               valueColor: const AlwaysStoppedAnimation(Colors.red),
               value: double.parse(airQualityValue ?? "0") / 100,
             ),
           ),
           SizedBox(
-            width: 40,
+            width: 40.w,
             child: Center(
               child: Text(
                 airQualityValue ?? "",
-                style: const TextStyle(color: Colors.white, fontSize: 12),
+                style: TextStyle(color: Colors.white, fontSize: 12.sp),
               ),
             ),
           )
